d8c6d1a9f4f67549e556e6f4e80cdb092bdb425a
manav33 <49527111+manav33@users.noreply.github.com>
2025-06-30 22:58:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== "POST") {
     res.status(405).send("Method Not Allowed");
     return;
